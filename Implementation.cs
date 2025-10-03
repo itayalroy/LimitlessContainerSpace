b62536000fd4c7f236071c2e369343ac0ec7cf8c
itayalroy <itayalroy@gmail.com>
2025-10-03 21:56:39 +0300

    Update to v2.44

--- Implementation.cs
+++ Implementation.cs
@@ -9,7 +9,11 @@ namespace LimitlessContainerSpace
     {
         private static void Postfix(Container __instance)
         {
-            __instance.m_CapacityKG = 10000;
+            var currentCapacity = __instance.m_Capacity;
+            var prevCapacity = (long)currentCapacity.m_Units / 1000000000.0;
+            currentCapacity.m_Units = 10000L * 1000000000L;
+            __instance.m_Capacity = currentCapacity;
+            MelonLogger.Msg($"[LimitlessContainerSpace] Container capacity modified: {prevCapacity:F1} kg → {10000.0:F1} kg");
         }
     }
 
