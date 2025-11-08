b10cafb5bc41ce0075cdcde4ff8ae5d3d9b854aa
itayalroy <itayalroy@gmail.com>
2025-11-08 19:53:09 +0200

    rename author

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,14 +7,14 @@ using System.Runtime.InteropServices;
 //ModName, ModVersion, ModAuthor, and ModNamespace.ModClassInheritingFromMelonMod all need changed.
 
 [assembly: AssemblyTitle("BeachcombingDetector")]
-[assembly: AssemblyCopyright("Created by Atlas Lumi")]
+[assembly: AssemblyCopyright("Created by Itay Alroy")]
 
 //Version numbers in C# are a set of 1 to 4 positive integers separated by periods.
 //Mods typically use 3 numbers. For example: 1.2.1
 //The mod version need specified in three places.
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: MelonInfo(typeof(BeachcombingDetector.Implementation), "BeachcombingDetector", "1.0.0", "Atlas Lumi")]
+[assembly: MelonInfo(typeof(BeachcombingDetector.Implementation), "BeachcombingDetector", "1.0.0", "Itay Alroy")]
 
 //This tells MelonLoader that the mod is only for The Long Dark.
 [assembly: MelonGame("Hinterland", "TheLongDark")]
